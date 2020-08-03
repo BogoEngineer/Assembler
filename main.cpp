f9587f59ad92c3004fbe9ad465214cd243613a46
Sistemski softver <miloskat@gmail.com>
2020-08-03 14:27:04 -0700

    Test commit.

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout<<"\n\nHello World,\nWelcome to my first C ++ program on Ubuntu Linux\n\n"<<endl;    
+    return 0;
+}
